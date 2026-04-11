3b191fedf79a59584c204c8414925d1b666e9356
Medsaker2010 <h.a.rouis17@gmail.com>
2026-04-11 13:55:46 +0100

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-export default function Home() {
-  return (
-    <div style={{background:'#050505',minHeight:'100vh',color:'white',display:'flex',alignItems:'center',justifyContent:'center'}}>
-      <h1 style={{color:'#D4AF37',fontSize:'3rem'}}>YourTVSat VIP</h1>
-    </div>
-  );
-}
